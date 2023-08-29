5ab4c06ff807755dde74a8f5bf65c0aa1313a11b
Teodorus Nathaniel <nathankurnia26@gmail.com>
2023-08-29 21:18:18 +0700

    Day 7 AJAX

--- tokopedia/script.js
+++ tokopedia/script.js
@@ -18,25 +18,36 @@ console.log('script connected')
 const productList = document.getElementById('product-list')
 // console.log(productList, productList.innerHTML)
 // productList.innerHTML = product.innerHTML + 'asdfasdfs'
-const data = [
-  {
-    name: 'iPhone 1',
-    image: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    rating: 4.5,
-    stok: 90,
-    price: 'Rp 80.000',
-  },
-  {
-    name: 'iPhone 10',
-    image: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
-    rating: 5,
-    stok: 3,
-    price: 'Rp 6.000.000',
-  },
-]
-for (let i = 0; i < data.length; i++) {
-  const currentProduct = data[i]
-  productList.innerHTML += `<div class="product">
+const sum = 5 + 10
+
+// promise => asynchronous programming
+// then & catch
+// async & await => cara yang lebih baru
+async function getData(page, search) {
+  console.log('start get data')
+
+  const limit = 5
+  const skip = (page - 1) * limit
+  // (page - 1) * limit
+  // 1 => 0
+  // 2 => 5
+  // 3 => 10
+  // 4 => 15
+
+  let apiUrl = `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
+  if (search) {
+    apiUrl = `https://dummyjson.com/products/search?q=${search}`
+  }
+
+  const response = await fetch(apiUrl)
+  const data = await response.json()
+  const products = data.products
+  console.log(products)
+
+  productList.innerHTML = ''
+  for (let i = 0; i < products.length; i++) {
+    const currentProduct = products[i]
+    productList.innerHTML += `<div class="product">
   <!-- HEART BUTTON -->
   <button class="heart-button">
     <span class="material-symbols-outlined">favorite</span>
@@ -45,34 +56,70 @@ for (let i = 0; i < data.length; i++) {
   <!-- IMAGE -->
   <div class="product-image">
     <img
-      src="${currentProduct.image}"
+      src="${currentProduct.thumbnail}"
       alt=""
     />
   </div>
   <!-- PRODUCT DETAIL -->
   <div class="product-detail">
-    <span>${currentProduct.name}</span>
-    <span>${currentProduct.price}</span>
+    <span>${currentProduct.title}</span>
+    <span>$${currentProduct.price}</span>
     <div class="product-info">
       <div class="product-rating">
         <span class="material-symbols-outlined">star</span>
         <span>${currentProduct.rating}</span>
       </div>
-      <span>Stok ${currentProduct.stok}</span>
+      <span>Stok ${currentProduct.stock}</span>
     </div>
   </div>
   </div>`
-}
-
-const heartButtons = document.getElementsByClassName('heart-button')
-for (let i = 0; i < heartButtons.length; i++) {
-  const button = heartButtons[i]
-  function handleClick() {
-    console.log('toggling')
-    button.classList.toggle('active')
   }
-  button.addEventListener('click', handleClick)
+
+  const heartButtons = document.getElementsByClassName('heart-button')
+  for (let i = 0; i < heartButtons.length; i++) {
+    const button = heartButtons[i]
+    function handleClick() {
+      console.log('toggling')
+      button.classList.toggle('active')
+    }
+    button.addEventListener('click', handleClick)
+  }
 }
+console.log('above get data')
+let currentPage = 1
+getData(currentPage)
+console.log('below get data')
+
+const prevBtn = document.getElementById('prev-btn')
+const nextBtn = document.getElementById('next-btn')
+
+// anonymous function
+nextBtn.addEventListener('click', function () {
+  currentPage++
+  getData(currentPage)
+})
+
+const searchInput = document.getElementById('search')
+searchInput.addEventListener('keypress', function (event) {
+  // console.log('keypressed!', event)
+  if (event.key === 'Enter') {
+    console.log('enter is pressed!', searchInput.value)
+    getData(currentPage, searchInput.value)
+  }
+})
+
+// function onKeyPress(event) {
+//   console.log(event)
+// }
+// searchInput.addEventListener('keypress', onKeyPress)
+
+// function addEventListener(type, listener) {
+//   // ...
+//   const event = '' // ... ini event nya
+//   listener(event)
+//   // ...
+// }
+
 // let num = 5
 // num = num + 2
 // num += 2
